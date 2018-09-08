eec43a91709c95577467e34a4161f39263bb60da
Lee Hambley <leehambley@harrow.io>
2018-09-08 14:17:29 +0200

    remove retro-cabinet (SPA) out to it's own repo

--- retro-cabinet/src/types/store.ts
+++
@@ -1,9 +0,0 @@
-import { IState as ObjectDatabaseViewerState } from './ObjectDatabaseViewer';
-import { IState as RefSelectorState } from './RefSelector';
-import { IState as ServerURLState } from './ServerURL';
-
-export default interface IStoreState {
-  readonly server: ServerURLState,
-  readonly refSelector: RefSelectorState,
-  readonly odbViewer: ObjectDatabaseViewerState,
-}
