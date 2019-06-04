0cde9691efd8b1804fb83844dbbb047449156082
Lee Hambley <leehambley@harrow.io>
2019-06-04 23:36:49 +0200

    embed queryable interface in depot interface, dry.

--- framework/retro/queryable.go
+++ framework/retro/queryable.go
@@ -5,5 +5,8 @@ import "context"
 type MatcherResults interface{}
 
 type Queryable interface {
+	// For arbitrary queries of the storage space. MatcherResults
+	// can be drained/filtered to extract Checkpoints, Affixes or
+	// Events at various levels of granularity 
 	Matching(context.Context, Matcher) (MatcherResults, error)
 }
